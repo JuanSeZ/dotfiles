4421d0ccd80e3fa8b8bca14b1d0463ca71f89cd2
Juan Segundo Zanelli <juanse@192.168.1.19>
2025-07-08 19:32:20 -0300

    fresh restart 002

---
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -0,0 +1,8 @@
+local set = vim.opt_local
+
+-- file type plugin -- filetype == language
+
+set.shiftwidth = 2
+set.number = true
+set.relativenumber = true
+
---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,16 @@
+-- Custom Neovim Config by Juanse
+
+require("config.lazy")
+
+-- Source file
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+
+
+-- Highlight when yanking
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
