5007a4af72496dbf93633330a6fa7e7df273b8dc
Juan Segundo Zanelli <juanse@192.168.1.11>
2025-07-09 21:04:24 -0300

    fresh restart 004

---
+++ nvim/.config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,6 @@
+local set = vim.opt_local
+
+-- file type plugin -- filetype == language
+
+set.number = true
+set.relativenumber = true
--- nvim/.config/nvim/lua/config/plugins/lsp.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp.lua
@@ -16,8 +16,29 @@ return {
       { 'saghen/blink.cmp' },
     },
     config = function()
+      -- Configure diagnostic signs and virtual text
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN]  = "",
+            [vim.diagnostic.severity.HINT]  = "",
+            [vim.diagnostic.severity.INFO]  = "",
+          },
+        },
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          border = "rounded",
+          source = true,
+        },
+      })
       local capabilities = require('blink.cmp').get_lsp_capabilities()
-      require("lspconfig").lua_ls.setup { capabilities = capabilities }
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup { capabilities = capabilities }
+      lspconfig.basedpyright.setup { capabilities = capabilities }
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('my.lsp', {}),
         callback = function(args)
