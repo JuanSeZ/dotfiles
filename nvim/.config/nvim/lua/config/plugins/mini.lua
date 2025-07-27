329f203336f69f32ba2c6c960be964d68a9821a6
Juan Segundo Zanelli <juanse@192.168.1.3>
2025-07-26 21:09:51 -0300

    fresh restart 012

--- nvim/.config/nvim/lua/config/plugins/mini.lua
+++ nvim/.config/nvim/lua/config/plugins/mini.lua
@@ -1,9 +1,16 @@
 return {
-	{
-		'echasnovski/mini.nvim',
-		config = function()
-			local statusline = require 'mini.statusline' 
-			statusline.setup { use_icons = true }
-		end
-	}
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      local statusline = require("mini.statusline")
+      -- TODO: Keymaps para hacer bb y bc
+      local tabline = require("mini.tabline")
+      local animate = require("mini.animate")
+      local indent_scope = require("mini.indentscope")
+      statusline.setup({ use_icons = true })
+      tabline.setup({ use_icons = true })
+      animate.setup({ cursor = { enable = false } })
+      indent_scope.setup()
+    end,
+  },
 }
