ddc5545a87a8974433e4093df04a323e5e49c24f
JuanSeZ <zanelli38@gmail.com>
2026-01-23 20:22:55 -0300

    fresh restart 024

---
+++ lazy-nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    opts = {
+      style = "cool", -- Options: dark, darker, cool, deep, warm, warmer, light
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
