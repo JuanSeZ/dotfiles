b2cee41b4318f7cff82a249629628604f5182280
JuanSeZ <zanelli38@gmail.com>
2026-01-23 21:28:36 -0300

    fresh restart 026

---
+++ lazy-nvim/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    preset = "classic", -- Options: "classic", "modern", "helix"
+    -- You can also customize other aspects:
+    -- win = {
+    --   border = "single", -- "none", "single", "double", "rounded", "solid", "shadow"
+    --   padding = { 1, 2 }, -- extra window padding [top/bottom, right/left]
+    -- },
+    -- layout = {
+    --   width = { min = 20, max = 50 },
+    --   spacing = 3, -- spacing between columns
+    -- },
+  },
+}
